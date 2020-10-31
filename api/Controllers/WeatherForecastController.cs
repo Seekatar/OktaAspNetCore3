754cba988aa6497f71165d93deac8a436b30019c
Jim Wallace <seekatar@gmail.com>
2020-10-31 11:58:03 -0400

    WIP working

--- api/Controllers/WeatherForecastController.cs
+++ api/Controllers/WeatherForecastController.cs
@@ -28,12 +28,14 @@ namespace api.Controllers
         [Authorize(Policy = "MyPolicy")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var client = Request.HttpContext.User.Claims.SingleOrDefault( c => c.Value.StartsWith("casualty.datacapture.client"))?.Value;
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = $"{Summaries[rng.Next(Summaries.Length)]} for {client?.Split('.').Last()}"
             })
             .ToArray();
         }
--- api/Startup.cs
+++ api/Startup.cs
@@ -25,9 +25,11 @@ namespace api
     {
         private readonly OktaSettings _okta;
 
-        public GroupPolicyHandler(IOptions<OktaSettings> okta) {
+        public GroupPolicyHandler(IOptions<OktaSettings> okta)
+        {
             _okta = okta.Value;
         }
+        const string ScopeClaimType = "http://schemas.microsoft.com/identity/claims/scope";
 
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, GroupRequirement requirement)
         {
@@ -35,24 +37,46 @@ namespace api
             WriteLine($"   Okta Issuer is {_okta.Issuer}");
             foreach (var c in context.User.Claims)
             {
-                WriteLine($"   {c.ToString()}  from {c.Issuer}");
+                if (c.Type == ScopeClaimType) {
+                    WriteLine($"   scope: {c.Value}");
+                } else if (c.Type == "clients") {
+                    WriteLine($"   {c.ToString()}");
+                }
             }
 
-            if (context.User.HasClaim(c =>
+            var client = context.User.Claims.SingleOrDefault(c => c.Type == ScopeClaimType && c.Value.StartsWith(_okta.ScopePrefix))?.Value;
+            if (client != null)
             {
-                return true;
-            }))
-            {
-                context.Succeed(requirement);
+                /*
+                    Examples
+                    _okta.ScopePrefix = "casualty.datacapture.client"
+                    _okta.GroupPrefix = "CCC-DataCapture-Client"
+                    scope = casualty.datacapture.client.usaa
+                    group = CCC-DataCapture-Client-USAA-Group
+                */
+                var toMatch = $"{_okta.GroupPrefix}-{client.Replace(_okta.ScopePrefix,"").Trim('.').Replace('.','-')}-group".ToLowerInvariant();
+                if (context.User.Claims.Any( c => c.Type == "clients"
+                                             && c.Value.ToLowerInvariant() == toMatch))
+                {
+                    context.Succeed(requirement);
+                }
+                else
+                {
+                    WriteLine($"Didn't find group match for scope {client} ({toMatch})");
+                }
             }
+            else
+            {
+                WriteLine($"Didn't get only one scope with prefix {_okta.ScopePrefix}");
+            }
+
             return Task.CompletedTask;
         }
     }
     public class OktaSettings
     {
-        public OktaSettings()
-        {}
-
+        public string ScopePrefix { get; set; }
+        public string GroupPrefix { get; set; }
         public string Issuer { get; set; }
         public string Audience { get; set; }
         public override string ToString()
